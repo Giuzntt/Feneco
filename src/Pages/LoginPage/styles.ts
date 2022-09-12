48f38bc7ea097eb0814926474486211c732ebcad
Giulianno Zanetti <giuzntt@gmail.com>
2022-09-11 23:10:00 -0300

    Login

---
+++ src/Pages/LoginPage/styles.ts
@@ -0,0 +1,23 @@
+// import mui styled component
+
+import styled from "@emotion/styled";
+import { Box, Container } from "@mui/material";
+
+
+export const LoginContainer = styled(Container)`
+    margin: 0 auto;
+`;
+
+export const BoxLogin= styled(Box)`
+    margin: 0 auto;
+    display: grid;
+    flex-direction: column;
+    grid-template-columns: 1fr;
+    grid-gap: 15px;
+    align-items: center;
+    justify-content: center;
+    background-color: #f5f5f5;
+    `
+    
+
+
