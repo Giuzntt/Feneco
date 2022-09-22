d929c5fbe68528902f91ed5ebbb2d35c7c484871
Giuzntt <giuzntt@gmail.com>
2022-09-22 17:12:45 -0300

    Criando página ao usuário

--- src/Pages/HomePage/index.tsx
+++ src/Pages/HomePage/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, Grid, Stack, Typography } from "@mui/material";
+import { Box, Grid, Stack, Typography } from "@mui/material";
 import BasicCard from "../../components/Card";
 
 
